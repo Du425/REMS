aeae66e7375d9c253b6f8d7ba67c8e45f3ddc32a
7399 <1910674037@qq.com>
2021-10-30 16:18:28 +0800

    first commit

---
+++ src/test/java/com/du/rems/RemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.rems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
